8a1f1c15a53f03d9df34ebd31af8ab609ffcf1a6
drhenrywood <88190258+drhenrywood@users.noreply.github.com>
2024-07-25 13:17:16 +0000

    added some functions

---
+++ src/hypot/source.py
@@ -0,0 +1,25 @@
+# some list of requirements
+
+
+def hypot(a,b):
+    """Function to return hypotenuse
+
+    Args:
+        a (int,float): First side of triangle
+        b (int,float): Second side of triangle
+
+    Returns:
+        float: hypotenuse
+    """
+    return sqrt(a**2 + b**2)
+
+# sqrt function
+
+def sqrt(a):
+    """sqrt function
+
+    Args:
+        a (int,float): calcs sqrt
+    """
+    return(a**0.5)
+
